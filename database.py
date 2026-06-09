2a7625f79b7944ee952b98c57ab6e02750281754
VonGojtan <56790303+VonGojtan@users.noreply.github.com>
2026-06-09 20:01:51 -0300

    DBV1

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres.foeqzxazamtqsqdyxsgb:Ego%40Virtualcsa01@aws-1-us-west-2.pooler.supabase.com:5432/postgres?sslmode=require"
-
+DATABASE_URL = "postgresql+psycopg2://postgres.foeqzxazamtqsqdyxsgb:Ego%40Virtualcsa01@db.foeqzxazamtqsqdyxsgb.supabase.co:5432/postgres?sslmode=require"
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
