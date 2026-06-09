6307e48164f13c4a56b9031b5a818d0a5152c17f
VonGojtan <56790303+VonGojtan@users.noreply.github.com>
2026-06-08 23:14:31 -0300

    Atualizando senha

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:Ego@Virtualcsa01@db.foeqzxazamtqsqdyxsgb.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql+psycopg2://postgres:Ego%40@Virtualcsa01%40@db.foeqzxazamtqsqdyxsgb.supabase.co:5432/postgres"
 
 engine = create_engine(
     DATABASE_URL,
