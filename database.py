0c04bed0a23a4e754225457d1b0fa206bef8f3d3
VonGojtan <56790303+VonGojtan@users.noreply.github.com>
2026-06-08 23:04:54 -0300

    Database V1

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 DATABASE_URL = "postgresql+psycopg2://postgres:Ego@Virtualcsa01@db.foeqzxazamtqsqdyxsgb.supabase.co:5432/postgres"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
